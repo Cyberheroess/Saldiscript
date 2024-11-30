c5803443573ce0c41f551980d67bef06213ce879
Saldy <saldilinuxchch@gmail.com>
2024-11-30 19:44:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import tensorflow as tf
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 logging.basicConfig(filename='log_serangan.txt', level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
