d2d4572644dd80da851afc645ab8b2e139828847
Saldy <saldilinuxchch@gmail.com>
2024-12-03 06:36:15 +0800

    Update file_inclusion.py

--- modules/file_inclusion.py
+++ modules/file_inclusion.py
@@ -3,7 +3,6 @@ class FileInclusion:
         pass
 
     def include_file(self, url, file_name):
-        # Simulate file inclusion vulnerability
         file_url = f"{url}?file={file_name}"
         print(f"Attempting file inclusion on: {file_url}")
         return file_url
